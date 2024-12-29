130d9dd50906967424f26847de16e3becd826e10
nourhan <nourhansaeed707@gmail.com>
2024-12-30 01:54:47 +0200

    add rate limiter changes

--- jobms/src/main/java/com/example/jobms/job/service/Impl/JobServiceImpl.java
+++ jobms/src/main/java/com/example/jobms/job/service/Impl/JobServiceImpl.java
@@ -9,6 +9,7 @@ import com.example.jobms.job.model.Job;
 import com.example.jobms.job.repository.JobRepository;
 import com.example.jobms.job.service.JobService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -40,7 +41,8 @@ public class JobServiceImpl implements JobService {
 
     @Override
 //    @CircuitBreaker(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
-    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+//    @Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+    @RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAll() {
         System.out.println("Attempt: " + ++attempt);
         List<Job> jobs = jobRepository.findAll();
